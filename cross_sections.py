e9c039a5d4f3cc590b5548e7330f3ccd55f6464f
ZombiKoi <delafosse_loris@orange.fr>
2025-02-04 17:19:03 +0100

    Début d'une fonction pour calculer le nombre de coups

--- cross_sections.py
+++ cross_sections.py
@@ -15,14 +15,12 @@ def cosec(x):
     """
     return 1./np.sin(x)
 
-def yaxis(xaxis, plot_instructions, A1, Z1, A2, Z2, initial_v, total_spin):
+def rutherford_cross_sections(angles, A1, Z1, A2, Z2, E_COM):
     """
-    Computes theoretical cross-sections for a given collision.
+    Computes theoretical Rutherford cross-sections for a given collision.
 
-    xaxis: numpy array
+    angles: numpy array
         List of angles at which measurements are done, in radians.
-    plot_instructions: string
-        Determines the theoretical curve to plot (possible values: 'rutherford', 'mott').
     A1: int
         Mass number of the beam particle.
     Z1: int
@@ -31,44 +29,74 @@ def yaxis(xaxis, plot_instructions, A1, Z1, A2, Z2, initial_v, total_spin):
         Mass number of the target particle.
     Z2: int
         Charge number of the target particle.
-    initial_v: float
-        Initial velocity of the beam particle in the lab frame, in SI units.
+    E_COM: float
+        Center-of-mass energy in MeV.
+
+    Returns:
+        numpy array: List of theoretical Rutherford cross-sections, for each angle.
+    """
+
+    nb_entries = len(angles)
+    mu = cst.m_n * A1 * A2 / (A1 + A2)
+    initial_v = np.sqrt(E_COM*(10**6)*cst.e*2/mu)
+    k = mu * initial_v / cst.hbar
+    n = (Z1*Z2 * cst.e*cst.e) / (4*np.pi*cst.epsilon_0 * initial_v * cst.hbar)
+
+    cross_sections = np.zeros(nb_entries)
+
+    for index in range(nb_entries):
+        theta = angles[index]
+        cross_sections[index] = n*n / (4.*k*k) * cosec(theta/2.)**4 #SI units
+#        if 'absorption' in plot_instructions and A1 != A2:
+#            absorption_factor = np.exp(-np.abs(A1 - A2) / (A1 + A2))
+#            cross_sections[index] *= absorption_factor
+        cross_sections[index] *= 10**31 #conversion in mbarns
+
+    return cross_sections
+
+def mott_cross_sections(angles, A1, Z1, A2, Z2, E_COM, total_spin):
+    """
+    Computes theoretical Mott cross-sections for a given collision.
+
+    angles: numpy array
+        List of angles at which measurements are done, in radians.
+    A1: int
+        Mass number of the beam particle.
+    Z1: int
+        Charge number of the beam particle.
+    A2: int
+        Mass number of the target particle.
+    Z2: int
+        Charge number of the target particle.
+    E_COM: float
+        Center-of-mass energy in MeV.
     total_spin: float
         Total spin of the target/beam atom when they are indistinguishable.
 
     Returns:
-        numpy array: List of theoretical cross-sections, for each angle.
+        numpy array: List of theoretical Mott cross-sections, for each angle.
     """
 
-    nb_entries = len(xaxis)
+    nb_entries = len(angles)
     mu = cst.m_n * A1 * A2 / (A1 + A2)
+    initial_v = np.sqrt(E_COM*(10**6)*cst.e*2/mu)
     k = mu * initial_v / cst.hbar
     n = (Z1*Z2 * cst.e*cst.e) / (4*np.pi*cst.epsilon_0 * initial_v * cst.hbar)
 
-    yaxis = np.zeros(nb_entries)
+    cross_sections = np.zeros(nb_entries)
 
-    if 'rutherford' in plot_instructions:
-        for index in range(nb_entries):
-            theta = xaxis[index]
-            yaxis[index] = n*n / (4.*k*k) * cosec(theta/2.)**4 #SI units
-            if A1 != A2:
-                absorption_factor = np.exp(-np.abs(A1 - A2) / (A1 + A2))
-                yaxis[index] *= absorption_factor
-            yaxis[index] *= 10**28 #conversion in barns
+    for index in range(nb_entries):
+        theta = angles[index]
+        cross_sections[index] = n*n / (4.*k*k) * ( cosec(theta/2.)**4 + sec(theta/2.)**4 + 2*(-1)**(2*total_spin)/(2*total_spin+1) * np.cos(n * np.log(np.tan(theta/2.)**2)) * cosec(theta/2.)**2 * sec(theta/2.)**2 ) #SI units
+        cross_sections[index] *= 10**31 #conversion in mbarns
 
-    if 'mott' in plot_instructions:
-        for index in range(nb_entries):
-            theta = xaxis[index]
-            yaxis[index] = n*n / (4.*k*k) * ( cosec(theta/2.)**4 + sec(theta/2.)**4 + 2*(-1)**(2*total_spin)/(2*total_spin+1) * np.cos(n * np.log(np.tan(theta/2.)**2)) * cosec(theta/2.)**2 * sec(theta/2.)**2 ) #SI units
-            yaxis[index] *= 10**28 #conversion in barns
+    return cross_sections
 
-    return yaxis
-
-def plot(xaxis, yaxes, linetypes, labels, xlabel, ylabel):
+def plot(angles, yaxes, linetypes, labels, xlabel, ylabel):
     """
     Plots a graph
 
-    xaxis: numpy array
+    angles: numpy array
         List of angles at which measurements are done, in radians.
     yaxes: list of numpy arrays
         List of curves to plot.
@@ -86,7 +114,7 @@ def plot(xaxis, yaxes, linetypes, labels, xlabel, ylabel):
     """
 
     nb_curves = len(yaxes)
-    xaxis *= 180/np.pi #conversion in degrees
+    angles *= 180/np.pi #conversion in degrees
 
     plt.rc('font',  family='serif')  # Type of font 
     plt.rc('xtick', labelsize='20')  # Size of the xtick label 
@@ -95,12 +123,14 @@ def plot(xaxis, yaxes, linetypes, labels, xlabel, ylabel):
     plt.rc('legend', framealpha='1') # Transparency of the legend frame
     plt.rc('legend', fontsize='23')  # Size of the legend
     plt.rc('grid', linestyle='--')   # Grid formed by dashed lines 
-    plt.rcParams.update({ "text.usetex": True }) # Using LaTex style for text and equation
+    plt.rcParams.update(
+        { "text.usetex": True } # Using LaTex style for text and equation
+    )
     
     fig, ( fig1 ) = plt.subplots( figsize=(8, 6) )
     
     for index in range(nb_curves):
-        fig1.plot(xaxis, yaxes[index], linetypes[index], label=labels[index])
+        fig1.plot(angles, yaxes[index], linetypes[index], label=labels[index])
 
     fig1.set_xlabel(xlabel, fontsize=23)
     fig1.set_ylabel(ylabel, fontsize=23)
@@ -109,21 +139,60 @@ def plot(xaxis, yaxes, linetypes, labels, xlabel, ylabel):
     
     plt.tight_layout()
     plt.show()
+    #plt.savefig('fig.pdf')
+
+def number_of_counts(detector_angle, det_solid_angle, particle_nb, target_density, A1, Z1, A2, Z2, E_COM):
+    """
+    Computes the theoretical number of counts for the detected energy (Rutherford scattering).
+
+    detector angle: float
+        Angle at which the detector is placed in the lab frame, in radians.
+    det_solid_angle: float
+        Solid angle occupied by the detector in the lab frame, in steradians.
+    particle_nb: int
+        Number of particles in the incident beam.
+    target_density: float
+        Number of scattering centres per squared meter.
+    A1: int
+        Mass number of the beam particle.
+    Z1: int
+        Charge number of the beam particle.
+    A2: int
+        Mass number of the target particle.
+    Z2: int
+        Charge number of the target particle.
+    E_COM: float
+        Center-of-mass energy in MeV.
+
+    Returns:
+        float: Detected energy.
+        int: Number of counts.
+    """
+    gamma = A1/A2
+    theta_COM = np.arcsin( np.sin(detector_angle) * (gamma * np.cos(detector_angle) + np.sqrt(1. - gamma*gamma*np.sin(detector_angle)**2)) ) #angle in the center-of-mass frame
+    det_energy = A1*A1*E_COM / (A2*(A1+A2)) * ( gamma * np.cos(detector_angle) + np.sqrt(1. - gamma*gamma*np.sin(detector_angle)**2) ) / (gamma*gamma) #detected energy
+    [cross_section_COM] = rutherford_cross_sections([theta_COM], A1, Z1, A2, Z2, E_COM)
+    #takes the cross-section to the lab frame
+    nb_counts = det_solid_angle * cross_section * particle_nb * target_density
+    return det_energy, nb_counts
 
 
 theta_start = 5 * np.pi/180 #initial angle for plot in radians
 theta_stop = 175* np.pi/180 #final angle for plot in radians
-xaxis = np.linspace(theta_start, theta_stop, 10000)
-A1,Z1,A2,Z2 = 12,6,12,6 #collision on carbon
-E_MeV = 35. #initial kinetic energy in the lab frame, in MeV
-initial_v = np.sqrt(E_MeV*(10**6)*cst.e*2/(A1*cst.m_n))
-total_spin = 6
+angles = np.linspace(theta_start, theta_stop, 10000)
+E_COM = 4. #center-of-mass energy in MeV
 
-c_yaxis = yaxis(xaxis, ['rutherford'], A1, Z1, A2, Z2, initial_v, total_spin)
-A1,Z1,A2,Z2 = 12,6,197,79 #collision on gold
-au_yaxis = yaxis(xaxis, ['rutherford'], A1, Z1, A2, Z2, initial_v, total_spin)
+A1,Z1,A2,Z2 = 12,6,12,6 #collision on carbon
+#c_yaxis = rutherford_cross_sections(angles, A1, Z1, A2, Z2, E_COM)
+#A1,Z1,A2,Z2 = 12,6,197,79 #collision on gold
+#au_yaxis = rutherford_cross_sections(angles, A1, Z1, A2, Z2, E_COM)
+
+#spin0_yaxis = mott_cross_sections(angles, A1, Z1, A2, Z2, E_COM, 0)
+#spin1_yaxis = mott_cross_sections(angles, A1, Z1, A2, Z2, E_COM, 1)
+#spin2_yaxis = mott_cross_sections(angles, A1, Z1, A2, Z2, E_COM, 2)
 
 xlabel = 'Scattering angle (°)'
-ylabel = 'Cross-section (b)'
-plot(xaxis, [au_yaxis, c_yaxis], ['r--','k--'], ['Gold','Carbon'], xlabel, ylabel)
+ylabel = 'Cross-section (mb/sr)'
+#plot(angles, [au_yaxis, c_yaxis], ['r--','k--'], ['Gold','Carbon'], xlabel, ylabel)
+#plot(angles, [spin0_yaxis, spin1_yaxis, spin2_yaxis], ['k-','b--','r:'], ['I=0','I=1','I=2'], xlabel, ylabel)
 
